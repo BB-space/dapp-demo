566a78379a8e2a7dab3060a2f7b10cbbe90c7a48
Sangwoo Han <sangwoo.han00@gmail.com>
2018-02-26 15:40:03 +0900

    remove deploy script for tokens

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,20 +1,6 @@
-const Tulip = artifacts.require("./Tulip.sol");
-const TokenSale = artifacts.require("./TokenSale.sol");
 const OddEven = artifacts.require("./OddEven.sol");
-const Sha3Test = artifacts.require("./Sha3Test.sol");
 
 
 module.exports = function(deployer, network, accounts) {
-	deployer.deploy(Sha3Test);
-	deployer
-		.deploy(Tulip)
-		.then(async () => {
-			await deployer.deploy(TokenSale, Tulip.address, 1000, web3.eth.coinbase);
-			await Tulip.at(Tulip.address).mint(web3.eth.coinbase, web3.toWei(1000000));
-
-			await Tulip.at(Tulip.address).approve(TokenSale.address, web3.toWei(500000));
-
-			await deployer.deploy(OddEven, Tulip.address);
-			await Tulip.at(Tulip.address).mint(OddEven.address, web3.toWei(1000000));
-		});
+	deployer.deploy(OddEven, Tulip.address);
 };
