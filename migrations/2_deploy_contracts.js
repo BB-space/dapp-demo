820de5ee1cfef82fedd025c39d63e7e4c1a5dc9e
Sangwoo Han <sangwoo.han00@gmail.com>
2018-02-26 15:41:27 +0900

    oddeven deploy param

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,5 +2,5 @@ const OddEven = artifacts.require("./OddEven.sol");
 
 
 module.exports = function(deployer, network, accounts) {
-	deployer.deploy(OddEven, Tulip.address);
+	deployer.deploy(OddEven);
 };
